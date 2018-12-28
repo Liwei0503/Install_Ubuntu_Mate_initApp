fec651834c61da7a33bb152aeec865b6dac537e3
Liwei0503 <liwei0503@gmail.com>
2018-12-28 18:23:50 +0800

    Create Install_Ubuntu_Mate_initApp.sh

---
+++ Install_Ubuntu_Mate_initApp.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y nano vim git cmake tree chromium-browser curl
+sudo apt-get install -y git make gcc ant openjdk-8-jdk unzip golang-go unzip gddrescue xz-utils gnome-disk-utility
+sudo pip install python-can
+sudo apt-get -y install python-pip
+sudo apt-get -y install can-utils
+sudo apt-get install thonny
